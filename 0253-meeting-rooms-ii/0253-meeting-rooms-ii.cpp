865bafce98bffdc78d4e755f979e9fbac4117092
Arpit Goyal <arpit.go4@gmail.com>
2024-03-07 13:40:36 +0530

    Time: 16 ms (36.78%), Space: 15.2 MB (72.66%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.cpp
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.cpp
@@ -4,41 +4,29 @@ public:
     // Time: O(NlogN)
     // Space: O(N)
     
-    // align different meeting into groups
-    // number of groups is the number of conference rooms required
-    
-    // each conference room will have a separate timeline
-    // align all meetings in different timelines
-    // number of timelines required for none of the meeting to conflict is the 
-    // number of conference rooms required
-    
-    bool meeting_conflict(vector<int>& v1, vector<int>& v2) {
-        if (v1[0] > v2[0])
-            swap(v1, v2);
+    int minMeetingRooms(vector<vector<int>>& intervals) {
+        int N = intervals.size();
         
-        return v1[1] > v2[0];
-    }
-    
-    int minMeetingRooms(vector<vector<int>>& A) {
-        int N = A.size();
-        sort(A.begin(), A.end(), [&](const auto& v1, const auto& v2) {
-            return v1[0] < v2[0];                                                    // sort by start-time
-        });
-        
-        auto comp = [&](const auto& v1, const auto& v2) {
-            return v1[1] > v2[1];
-        };
-        priority_queue<vector<int>, vector<vector<int>>, decltype(comp)> pq(comp);   // min-heap by end-time
-        
-        pq.push(A[0]);
-        for (int i = 1; i < N; i++) {
-            auto curr = pq.top();
-            if (!meeting_conflict(curr, A[i]))
-                pq.pop();
-            
-            pq.push(A[i]);
+        int j = 0;
+        vector<pair<int,int>> A(2*N);
+        for (int i = 0; i < N; i++) {
+            A[j++] = { intervals[i][0], 1 };
+            A[j++] = { intervals[i][1], -1 };
         }
         
-        return pq.size();
+        sort(A.begin(), A.end(), [&](const auto& p1, const auto& p2) {
+            if (p1.first == p2.first)
+                return p1.second < p2.second;
+            return p1.first < p2.first;
+        });
+        
+        int max_count = 0;
+        int count = 0;
+        for (int i = 0; i < 2*N; i++) {
+            count += A[i].second;
+            max_count = max(max_count, count);
+        }
+        
+        return max_count;
     }
 };
