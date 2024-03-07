1b096a7589b3e833a24bc6a7ef8cb1ef74d2af05
Arpit Goyal <arpit.go4@gmail.com>
2024-03-07 13:31:09 +0530

    Time: 15 ms (43.47%), Space: 16.4 MB (19.04%) - LeetHub

---
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    
+    // Time: O(NlogN)
+    // Space: O(N)
+    
+    // align different meeting into groups
+    // number of groups is the number of conference rooms required
+    
+    // each conference room will have a separate timeline
+    // align all meetings in different timelines
+    // number of timelines required for none of the meeting to conflict is the 
+    // number of conference rooms required
+    
+    bool meeting_conflict(vector<int>& v1, vector<int>& v2) {
+        if (v1[0] > v2[0])
+            swap(v1, v2);
+        
+        return v1[1] > v2[0];
+    }
+    
+    int minMeetingRooms(vector<vector<int>>& A) {
+        int N = A.size();
+        sort(A.begin(), A.end(), [&](const auto& v1, const auto& v2) {
+            return v1[0] < v2[0];
+        });
+        
+        auto comp = [&](const auto& v1, const auto& v2) {
+            return v1[1] > v2[1];
+        };
+        priority_queue<vector<int>, vector<vector<int>>, decltype(comp)> pq(comp);   // min-heap by end-time
+        
+        pq.push(A[0]);
+        for (int i = 1; i < N; i++) {
+            auto curr = pq.top();
+            if (!meeting_conflict(curr, A[i]))
+                pq.pop();
+            
+            pq.push(A[i]);
+        }
+        
+        return pq.size();
+    }
+};
