5a5e1ed9d8ebad80cbb3dd3b6c3917cecdbaa54e
Arpit Goyal <arpit.go4@gmail.com>
2024-03-07 13:43:06 +0530

    Time: 19 ms (19.39%), Space: 16.7 MB (15.85%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.cpp
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.cpp
@@ -1,29 +1,15 @@
 class Solution {
 public:
-    
-    // Time: O(NlogN)
-    // Space: O(N)
-    
-    int minMeetingRooms(vector<vector<int>>& intervals) {
-        int N = intervals.size();
-        
-        int j = 0;
-        vector<pair<int,int>> A(2*N);
-        for (int i = 0; i < N; i++) {
-            A[j++] = { intervals[i][0], 1 };
-            A[j++] = { intervals[i][1], -1 };
+    int minMeetingRooms(vector<vector<int>>& A) {
+        map<int,int> line;
+        for (auto& v : A) {
+            line[v[0]]++;
+            line[v[1]]--;
         }
         
-        sort(A.begin(), A.end(), [&](const auto& p1, const auto& p2) {
-            if (p1.first == p2.first)
-                return p1.second < p2.second;
-            return p1.first < p2.first;
-        });
-        
-        int max_count = 0;
-        int count = 0;
-        for (int i = 0; i < 2*N; i++) {
-            count += A[i].second;
+        int max_count = 0, count = 0;
+        for (auto& p : line) {
+            count += p.second;
             max_count = max(max_count, count);
         }
         
