4958f9bdce336a2cc5aa8c88cefb5441de0a1136
Arpit Goyal <arpit.go4@gmail.com>
2024-03-08 15:29:52 +0530

    Time: 120 ms (37.70%), Space: 45.3 MB (17.82%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -4,62 +4,79 @@ public:
     // Time: O(N * M * log(N * M))
     // Space: O(N * M)
     
-    // weight for an edge can defined as, max(W[i,j], Route[i,j])
-    
-    #define INF INT_MAX
-    
-    typedef tuple<int,int,int> state;
-    
-    vector<vector<int>> dist, vis;
-    
     vector<pair<int,int>> moves{
-        { -1, 0 },
         { 1, 0 },
-        { 0, -1 },
+        { -1, 0 },
         { 0, 1 },
+        { 0, -1 },
     };
     
-    void dijkstra(int src_i, int src_j, vector<vector<int>>& G, int N, int M) {
-        auto comp = [&](const state& t1, const state& t2) {
-            return get<0>(t1) > get<0>(t2);
-        };
-        priority_queue<state, vector<state>, decltype(comp)> pq(comp);              // min-heap
+    vector<int> P, S, W;
+    
+    int get_idx(int i, int j, int M) {
+        return i * M + j;
+    }
+    
+    int root(int x) {
+        if (P[x] != x)
+            P[x] = root(P[x]);
         
-        dist[src_i][src_j] = 0;
-        pq.push({ dist[src_i][src_j], src_i, src_j });
+        return P[x];
+    }
+    
+    void union_set(int x, int y) {
+        int root_x = root(x);
+        int root_y = root(y);
         
-        while (!pq.empty()) {
-            state s = pq.top();
-            pq.pop();
-            
-            int d, i, j;
-            tie(d, i, j) = s;
-            
-            if (vis[i][j] != 0)
-                continue;
-            vis[i][j] = 1;
-            
-            for (auto& move : moves) {
-                int n_i = move.first + i, n_j = move.second + j;
-                if (n_i >= 0 && n_i < N && n_j >= 0 && n_j < M) {
-                    if (dist[n_i][n_j] > max(dist[i][j], abs(G[n_i][n_j]-G[i][j]))) {
-                        dist[n_i][n_j] = max(dist[i][j], abs(G[n_i][n_j]-G[i][j]));
-                        pq.push({ dist[n_i][n_j], n_i, n_j });
-                    }
-                }
-            }
+        if (root_x == root_y)
+            return;
+        
+        if (S[root_x] < S[root_y]) {
+            swap(root_x, root_y);
+            swap(x, y);
         }
+        
+        P[root_y] = P[root_x];
+        S[root_x] += S[root_y];
+        S[root_y] = -1;
     }
     
     int minimumEffortPath(vector<vector<int>>& G) {
         int N = G.size();
         int M = G[0].size();
         
-        dist.assign(N, vector<int>(M, INF));
-        vis.assign(N, vector<int>(M, 0));
+        P.assign(N*M, -1);
+        S.assign(N*M, 1);
+        for (int i = 0; i < N*M; i++)
+            P[i] = i;
         
-        dijkstra(0, 0, G, N, M);
+        vector<tuple<int,int,int>> edges;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                for (auto& move : moves) {
+                    int n_i = move.first + i, n_j = move.second + j;
+                    if (n_i >= 0 && n_i < N && n_j >= 0 && n_j < M)
+                        edges.push_back({ get_idx(i, j, M), get_idx(n_i, n_j, M), abs(G[i][j]-G[n_i][n_j]) });
+                }
+            }
+        }
         
-        return dist[N-1][M-1];
+        sort(edges.begin(), edges.end(), [&](const auto& t1, const auto& t2) {
+            return get<2>(t1) < get<2>(t2);
+        });
+        
+        int src = get_idx(0, 0, M), dest = get_idx(N-1, M-1, M);
+        int u, v, w;
+        for (auto& edge : edges) {
+            tie(u, v, w) = edge;
+            if (root(u) != root(v)) {
+                union_set(u, v);
+            }
+            
+            if (root(src) == root(dest))
+                return w;
+        }
+        
+        return 0;
     }
 };
