4fbfcf5ced7ceedb4e1fdd8bbcffa0ffc59f019d
Arpit Goyal <arpit.go4@gmail.com>
2025-03-13 00:10:23 +0530

    Time: 116 ms (15.05%), Space: 37.5 MB (21.67%) - LeetHub

---
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+
+    // Time: O(N^2)
+    // Space: O(N^2)
+
+    vector<vector<int>> VIS;
+    vector<vector<int>> directions{
+        { -1, 1 },       // north-east
+        { 0, 1 },        // east
+        { 1, 1 },        // south-east
+        { -1, 0 },       // north
+        { 1, 0 },         // south
+        { 0, -1 },       // west
+        { -1, -1 },      // north-west
+        { 1, -1 }       // south-west
+    };
+
+    int shortestPathBinaryMatrix(vector<vector<int>>& G) {
+        if (G[0][0] != 0)
+            return -1;
+
+        int N = G.size();
+        VIS.assign(N, vector<int>(N, 0));
+
+        queue<tuple<int,int,int>> q;
+        q.push({ 0, 0, 1 });
+            
+        int min_path_len = INT_MAX;
+        while (!q.empty()) {
+            tuple<int,int,int> t = q.front();
+            q.pop();
+
+            int i, j, dist;
+            tie(i, j, dist) = t;
+
+            if (i == N-1 && j == N-1)
+                return dist;
+
+            if (VIS[i][j] != 0)
+                continue;
+            VIS[i][j] = 1;
+
+            for (vector<int>& d : directions) {
+                int x = d[0], y = d[1];
+                if (i+x >= 0 && i+x < N && j+y >= 0 && j+y < N && G[i+x][j+y] == 0)
+                    q.push({ i+x, j+y, dist+1 });
+            }
+
+        }
+
+        return min_path_len == INT_MAX ? -1 : min_path_len;
+    }
+};
