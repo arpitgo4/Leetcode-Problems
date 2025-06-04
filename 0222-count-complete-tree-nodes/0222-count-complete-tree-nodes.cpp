a68e51932aed6a60c141d31a0ecf56f263969e0a
Arpit Goyal <arpit.go4@gmail.com>
2025-06-04 20:44:20 +0530

    Problem (Easy): 0222-count-complete-tree-nodes, Time: 0 ms (100%), Space: 31.5 MB (11.14%)

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -11,12 +11,57 @@
  */
 class Solution {
 public:
-    int countNodes(TreeNode* node) {
-        if (node == NULL)
+
+    // Time: O((logN)^2)
+    // Space: O(logN)
+
+    int countNodes(TreeNode* root) {
+        if (root == NULL)
             return 0;
-        
-        return 1 + 
-            countNodes(node->left) + 
-            countNodes(node->right);
+
+        int low = 1, high = 5 * 1e4;
+        while (low < high) {
+            int mid = ((high - low) / 2) + low;
+            // cout << "mid: " << mid << endl;
+            bool node_found = findNodeByIndex(root, mid);
+            if (node_found) {
+                low = mid+1;
+            } else {
+                high = mid;
+            }
+        }
+
+        return high;
+    }
+
+    bool findNodeByIndex(TreeNode* root, int target_index) {
+        stack<int> node_vals_till_par;
+        int curr_val = target_index;
+        while (curr_val > 0) {
+            node_vals_till_par.push(curr_val);
+            curr_val = (curr_val - 1) / 2;
+        }
+
+        bool target_node_found = dfs(root, 0, node_vals_till_par);
+        return target_node_found;
+    }
+
+    bool dfs(TreeNode* root, int curr_node_idx, stack<int>& node_vals) {
+        if (root == NULL)
+            return false;
+        if (node_vals.empty()) {
+            return true;
+        }
+
+        int left_idx = curr_node_idx * 2 + 1;
+        int right_idx = left_idx + 1;
+
+        if (left_idx == node_vals.top()) {
+            node_vals.pop();
+            return dfs(root->left, left_idx, node_vals);
+        } else {
+            node_vals.pop();
+            return dfs(root->right, right_idx, node_vals);
+        }
     }
 };
