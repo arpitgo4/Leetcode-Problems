ece6b4d88bac9bd79623ae6325dd2ff7ef576021
Arpit Goyal <arpit.go4@gmail.com>
2025-06-09 16:51:47 +0530

    Problem (Medium): 0739-daily-temperatures, Time: 13 ms (89.85%), Space: 102.9 MB (69.11%)

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -9,9 +9,10 @@ public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
         int temperature_cnt = temperatures.size();
         vector<int> result(temperature_cnt, 0);
-        stack<int> mono_stack;                                // monotonically decreasing stack
+        stack<int> mono_stack;                                          // monotonically decreasing stack
         for (int i = temperature_cnt-1; i >= 0; i--) {
-            while (!mono_stack.empty() && temperatures[mono_stack.top()] <= temperatures[i]) {
+            while (!mono_stack.empty() && 
+                    temperatures[mono_stack.top()] <= temperatures[i]) {
                 mono_stack.pop();
             }
 
