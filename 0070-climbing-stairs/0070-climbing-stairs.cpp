076e69c6d0a7d7b8d3b978f799e68108bc08c782
Arpit Goyal <arpit.go4@gmail.com>
2025-06-09 17:39:31 +0530

    Problem (Easy): 0070-climbing-stairs, Time: 0 ms (100%), Space: 8.7 MB (17.29%)

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+
+    // Time: O(N)
+    // Space: O(N)
+
+    int climbStairs(int step_cnt) {
+        vector<int> dp(step_cnt+1, -1);
+
+        return findWaysToClimb(0, step_cnt, dp);
+    }
+
+    int findWaysToClimb(int curr_step, int step_cnt, vector<int>& dp) {
+        if (curr_step > step_cnt)
+            return INT_MAX;
+        if (curr_step == step_cnt)
+            return 1;
+        if (dp[curr_step] != -1)
+            return dp[curr_step];
+
+        int total_ways = 0;
+        int ways_by_one = findWaysToClimb(curr_step + 1, step_cnt, dp);
+        if (ways_by_one != INT_MAX) {
+            total_ways += ways_by_one;
+        }
+        int ways_by_two = findWaysToClimb(curr_step + 2, step_cnt, dp); 
+        if (ways_by_two != INT_MAX) {
+            total_ways += ways_by_two;
+        }
+
+        return dp[curr_step] = total_ways;
+    }
+};
