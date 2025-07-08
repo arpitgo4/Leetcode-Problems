10e4a279dde47f5f9f8877ac6435f55f6e03285c
Arpit Goyal <arpit.go4@gmail.com>
2025-07-08 13:59:08 +0530

    Time: 3 ms (70.93%), Space: 14.5 MB (59.97%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+
+    // Time: O(NlogN)
+    // Space: O(1)
+
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int num_cnt = nums.size();
+
+        vector<int> order_nums(num_cnt);
+        iota(order_nums.begin(), order_nums.end(), 0);
+
+        auto cmp = [&](auto& a, auto& b) {
+            return nums[a] < nums[b];
+        };
+        sort(order_nums.begin(), order_nums.end(), cmp);
+        
+        int i = 0, j = num_cnt-1;
+        while (i < j) {
+            int sum = nums[order_nums[i]] + nums[order_nums[j]];
+            if (sum == target) {
+                return vector<int>{ order_nums[i], order_nums[j] };
+            } else if (sum < target) {
+                i++;
+            } else {
+                j--;
+            };
+        }
+
+        return { -1, -1 };
+    }
+};
