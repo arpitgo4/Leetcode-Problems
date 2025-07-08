11552797e5a35da4b1f502b9c55877940cdb9be6
Arpit Goyal <arpit.go4@gmail.com>
2025-07-08 14:02:27 +0530

    Time: 0 ms (100%), Space: 14.7 MB (58.58%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -7,19 +7,24 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int num_cnt = nums.size();
 
-        vector<int> order_nums(num_cnt);
-        iota(order_nums.begin(), order_nums.end(), 0);
+        vector<int> order_nums_idx(num_cnt);
+        iota(order_nums_idx.begin(), order_nums_idx.end(), 0);
 
         auto cmp = [&](auto& a, auto& b) {
             return nums[a] < nums[b];
         };
-        sort(order_nums.begin(), order_nums.end(), cmp);
+        sort(order_nums_idx.begin(), order_nums_idx.end(), cmp);
         
         int i = 0, j = num_cnt-1;
         while (i < j) {
-            int sum = nums[order_nums[i]] + nums[order_nums[j]];
+            int a = nums[order_nums_idx[i]];
+            int b = nums[order_nums_idx[j]];
+            int sum = a + b;
             if (sum == target) {
-                return vector<int>{ order_nums[i], order_nums[j] };
+                return vector<int>{ 
+                    order_nums_idx[i], 
+                    order_nums_idx[j] 
+                };
             } else if (sum < target) {
                 i++;
             } else {
