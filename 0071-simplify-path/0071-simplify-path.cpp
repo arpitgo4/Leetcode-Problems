e7cc3970f0fda63a0d3dbdc506d6ade258048ec7
Arpit Goyal <arpit.go4@gmail.com>
2026-07-01 17:51:06 +0530

    Time: 3 ms (62.82%), Space: 11.2 MB (63.84%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.cpp
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -4,55 +4,26 @@ public:
     // Time: O(N)
     // Space: O(N)
     string simplifyPath(string path) {
-        stack<string> str_stack;
+        vector<string> str_stack;
+        stringstream ss(path);
         string curr_str;
-        for (int i = 0; i < path.length(); i++) {
-            char curr_char = path[i];
-            if (curr_char == '/') {
-                if (curr_str != "") {
-                    str_stack.push(curr_str);
-                    curr_str = "";
+        while (getline(ss, curr_str, '/')) {
+            if (curr_str == "." || curr_str.empty()) {
+                continue;
+            } else if (curr_str == "..") {
+                if (!str_stack.empty()) {
+                    str_stack.pop_back();
                 }
-            } else {
-                curr_str += curr_char;
-            }
-        }
-
-        if (curr_str != "") {
-            str_stack.push(curr_str);
-        }
-
-        int par_dir_cnt = 0;
-        stack<string> ordering_stack;
-        while (!str_stack.empty()) {
-            string curr_str = str_stack.top();
-            str_stack.pop();
-
-            if (curr_str == "..") {
-                par_dir_cnt++;
-                continue;
-            } else if (curr_str == ".") {
-                continue;
-            }
-
-            if (par_dir_cnt != 0) {
-                par_dir_cnt--;
-                continue;
-            }
-
-            if (curr_str != "") {
-                ordering_stack.push(curr_str);
+            } else {                            // valid directory name
+                str_stack.push_back(curr_str);
             }
         }
 
         string simplified_path = "";
-        while (!ordering_stack.empty()) {
-            string curr_str = ordering_stack.top();
-            ordering_stack.pop();
-
+        for (string& curr_str : str_stack) {
             simplified_path += "/" + curr_str;
         }
 
-        return simplified_path == "" ? "/" : simplified_path;
+        return simplified_path.empty() ? "/" : simplified_path;
     }
 };
