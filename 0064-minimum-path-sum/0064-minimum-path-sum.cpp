374ebffc62703162e16200f62f4e71803cb28560
Arpit Goyal <arpit.go4@gmail.com>
2026-07-01 16:28:53 +0530

    Time: 0 ms (100%), Space: 16.5 MB (16.35%) - LeetHub

---
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -0,0 +1,71 @@
+const int INF = INT_MAX;
+
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int row_cnt = grid.size();
+        int col_cnt = grid[0].size();
+
+        vector<vector<int>> dp(row_cnt, vector<int>(col_cnt, -1));
+
+        return findMinPathSum(
+            0,
+            0,
+            row_cnt,
+            col_cnt,
+            grid,
+            dp
+        );
+    }
+
+    int findMinPathSum(
+        int curr_row,
+        int curr_col,
+        int row_cnt,
+        int col_cnt,
+        vector<vector<int>>& grid,
+        vector<vector<int>>& dp
+    ) {
+        if (curr_row < 0 || curr_row == row_cnt ||
+            curr_col < 0 || curr_col == col_cnt) {
+            return INF;
+        }
+
+        if (curr_row == row_cnt-1 && curr_col == col_cnt-1) {
+            return grid[curr_row][curr_col];
+        }
+
+        if (dp[curr_row][curr_col] != -1) {
+            return dp[curr_row][curr_col];
+        }
+
+        int down_min_sum = findMinPathSum(
+            curr_row + 1,
+            curr_col,
+            row_cnt,
+            col_cnt,
+            grid,
+            dp
+        );
+        if (down_min_sum != INF) {
+            down_min_sum += grid[curr_row][curr_col];
+        }
+
+        int right_min_sum = findMinPathSum(
+            curr_row,
+            curr_col + 1,
+            row_cnt,
+            col_cnt,
+            grid,
+            dp
+        );
+        if (right_min_sum != INF) {
+            right_min_sum += grid[curr_row][curr_col];
+        }
+
+        return dp[curr_row][curr_col] = min(
+            down_min_sum,
+            right_min_sum
+        );
+    }
+};
