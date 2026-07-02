10b48f7a66c08391697e6cf530a74c3d1d02cca2
Arpit Goyal <arpit.go4@gmail.com>
2026-07-02 17:23:33 +0530

    Time: 52 ms (11.29%), Space: 92.1 MB (29.33%) - LeetHub

---
+++ 0901-online-stock-span/0901-online-stock-span.cpp
@@ -0,0 +1,27 @@
+class StockSpanner {
+public:
+    stack<pair<int,int>> mono_stack_;         // monotonically decreasing stack
+
+    StockSpanner() {
+        
+    }
+    
+    // Time: O(N)
+    // Space: O(N)
+    int next(int price) {
+        int span = 1;
+        while (!mono_stack_.empty() && mono_stack_.top().first <= price) {
+            span += mono_stack_.top().second;
+            mono_stack_.pop();
+        }
+
+        mono_stack_.push({ price, span });
+        return span;
+    }
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
