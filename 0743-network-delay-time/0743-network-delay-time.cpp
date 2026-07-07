f10f2918a76b2f41d349d227ee1a7c3a2981e27e
Arpit Goyal <arpit.go4@gmail.com>
2026-07-07 16:28:15 +0530

    Time: 60 ms (99.74%), Space: 44.2 MB (89.85%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -1,76 +1,86 @@
+using edge_t = pair<int,int>;
+using graph_t = vector<vector<edge_t>>;
+using state_t = pair<int,int>;
+
+const int INF = INT_MAX;
+ 
 class Solution {
 public:
 
     // Time: O(ElogV)
     // Space: O(V)
 
-    const int INF = INT_MAX;
+    int networkDelayTime(
+        vector<vector<int>>& connections,
+        int node_cnt,
+        int source_node
+    ) {
+        graph_t adj_list(node_cnt+1, vector<edge_t>());
 
-    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<vector<pair<int,int>>> adj_list(n+1, vector<pair<int,int>>());
-        for (auto& vec : times) {
-            int src = vec[0],
-                dest = vec[1],
-                duration = vec[2];
+        for (vector<int>& connection : connections) {
+            int source_node = connection[0];
+            int dest_node = connection[1];
+            int latency = connection[2];
 
-            adj_list[src].push_back({ dest, duration });
+            adj_list[source_node].push_back({ dest_node, latency });
         }
 
-        vector<int> dist(n+1, INF);
+        vector<int> least_latency(node_cnt+1, INF);
 
-        findSingleSourceShortestTimes(
+        findLeastLatencyFromSource(
             adj_list,
-            n,
-            k,
-            dist
+            node_cnt,
+            source_node,
+            least_latency
         );
 
-        int max_time_required = 0;
-        for (int i = 1; i <= n; i++) {
-            int d = dist[i];
-            if (i == k) {
-                continue;
+        bool all_nodes_reachable = true;
+        int max_latency = -1;
+        for (int i = 1; i <= node_cnt; i++) {
+            if (least_latency[i] == INF) {
+                all_nodes_reachable = false;
+                break;
             }
 
-            max_time_required = max(max_time_required, d);
+            max_latency = max(max_latency, least_latency[i]);
         }
 
-        return max_time_required == INF ? -1 : max_time_required;
+        return !all_nodes_reachable ? -1 : max_latency;
     }
 
-    void findSingleSourceShortestTimes(
-        vector<vector<pair<int,int>>>& adj_list,
-        int n,
-        int k,
-        vector<int>& dist
+    void findLeastLatencyFromSource(
+        graph_t& adj_list,
+        int node_cnt,
+        int source_node,
+        vector<int>& least_latency
     ) {
+        least_latency[source_node] = 0;
 
-        vector<int> vis(n+1, 0);
+        vector<int> vis(node_cnt+1, 0);
+
         auto cmp = [&](auto& s1, auto& s2) {
             return s1.first > s2.first;
         };
-        priority_queue<pair<int,int>, vector<pair<int,int>>, decltype(cmp)> node_queue(cmp);
+        priority_queue<state_t, vector<state_t>, decltype(cmp)> node_queue(cmp);
+        node_queue.push({ least_latency[source_node], source_node });
 
-        dist[k] = 0;
-        node_queue.push({ dist[k], k });
-        vis[k] = 1;
-
-        while (!node_queue.empty()) {
-            auto [ _, curr_node ] = node_queue.top();
+        while(!node_queue.empty()) {
+            auto [ curr_latency, curr_node ] = node_queue.top();
             node_queue.pop();
 
-            if (vis[curr_node] == 2)
+            if (vis[curr_node] != 0) {
                 continue;
-
-            for (auto& [ next_node, duration ] : adj_list[curr_node]) {
-                if (dist[next_node] > dist[curr_node] + duration) {
-                    dist[next_node] = dist[curr_node] + duration;
-                    node_queue.push({ dist[next_node], next_node });
-                    vis[next_node] = 1;
-                }
             }
 
-            vis[curr_node] = 2;
+            vis[curr_node] = 1;
+
+            for (edge_t& connection : adj_list[curr_node]) {
+                auto [ next_node, next_latency ] = connection;
+                if (least_latency[next_node] > least_latency[curr_node] + next_latency) {
+                    least_latency[next_node] = least_latency[curr_node] + next_latency;
+                    node_queue.push({ least_latency[next_node], next_node });
+                }
+            }
         }
     }
 };
